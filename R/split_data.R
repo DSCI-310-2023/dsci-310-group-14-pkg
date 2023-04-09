6cfd3961f1db58a9fa320c3f590b58d6ba4a0dd4
rhysong <jeffreys9973@gmail.com>
2023-04-09 12:09:52 -0700

    update functions to work with tests

--- R/split_data.R
+++ R/split_data.R
@@ -1,14 +1,14 @@
-#' Splits dataset into a training and testing dataset and writes it to a CSV
+#' Splits dataset into a training and testing dataset and saves it into 2 global variable
 #'
 #' @param data Specified dataframe
 #' @param proportion Specified split proportion
 #' @param specified_strata Specified variable that you are prediciting for
 #'
-#' @return function writes final dataframe into two dataframe
+#' @return function saves final dataframe into two global variables "data_testing", "data_training"
 #' @export
 #'
 #' @examples
-#' #split_data(dataset,0.7,"Is_All_Star")
+#' split_data(mtcars,0.7,"vs")
 split_data <- function(data,proportion,specified_strata) {
   if (!is.data.frame(data)) {
     stop("data should be a data frame")
@@ -17,11 +17,9 @@ split_data <- function(data,proportion,specified_strata) {
     stop("proportion should be of type numeric")
   }
   if (!is.character(specified_strata)) {
-    stop("specified_strata should be of type string/character")
+    stop("specified_strata should be of type character")
   }
   data_split <- rsample::initial_split(data, prop = proportion, strata = specified_strata)
-  data_training <- rsample::training(data_split)
-  data_testing <- rsample::testing(data_split)
-  readr::write_csv(data_training,file = here::here("data/processed/training_set.csv"))
-  readr::write_csv(data_testing,file = here::here("data/processed/testing_set.csv"))
+  data_training <<- rsample::training(data_split)
+  data_testing <<- rsample::testing(data_split)
 }
--- R/visualize_knn.R
+++ R/visualize_knn.R
@@ -1,35 +1,31 @@
-#' Visualizes a knn model dataframe
+#' Visualizes a knn K selector graph using accuracy and K dataframe
 #'
 #' @param data Specified dataframe
-#' @param x Specified x-axis
-#' @param y Specified y-axis
+#' @param x Specified K variable
+#' @param y Specified Accuracy variable
 #' @param title Specified title of knn plot
 #'
-#' @return Returns a plot using specified dataframe
+#' @return Returns a knn k selector plot using specified data
 #' @export
 #'
 #' @examples
-#' #visualization <- visualize_knn(data_fit,x = height,y = weight,title = "Prediction")
+#' K <- c(1,2,4,5,10,18,25)
+#' accuracy <- c(0.2,0.4,0.6,0.7,0.4,0.2,0.3)
+#' df <- data.frame(K,accuracy)
+#' visualize_knn(df,K,accuracy,"lethimcook")
 visualize_knn <- function(data, x, y, title) {
   if (!is.data.frame(data)) {
     stop("data should be a data frame")
-  }
-  if (!is.character(x)) {
-    stop("x-axis should be of type string/character")
-  }
-  if (!is.character(y)) {
-    stop("y-axis should be of type string/character")
   }
   if (!is.character(title)) {
     stop("title should be of type string/character")
   }
   plot <- ggplot2::ggplot(df) +
-  ggplot2::geom_line(aes(x = x, y = y)) +
-  ggplot2::geom_point() +
+  ggplot2::geom_line(ggplot2::aes(x = {{x}}, y = {{y}})) +
+  ggplot2::geom_point(ggplot2::aes(x = {{x}}, y = {{y}})) +
   ggplot2::labs(title = title)+
   ggplot2::xlab("Number of K") +
   ggplot2::ylab("Accuracy") +
-  ggplot2::theme(text = element_text(size = 18))
-
+  ggplot2::theme(text = ggplot2::element_text(size = 18))
     return(plot)
 }
